9d475d7e9fe4fc3892aa6f715fe47d8673b40066
Marcus Vinicius Nascimento Ferreira <dev.mvinicius@gmail.com>
2026-03-23 21:54:08 -0300

    feat: Implement image picker functionality in ImageViewer widget

--- lib/widgets/new_category/image_viewer.widget.dart
+++ lib/widgets/new_category/image_viewer.widget.dart
@@ -1,13 +1,33 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:minha_biblioteca/colors.dart';
 
-class ImageViewer extends StatelessWidget {
+class ImageViewer extends StatefulWidget {
   const ImageViewer({super.key});
+
+  @override
+  State<ImageViewer> createState() => _ImageViewerState();
+}
+
+class _ImageViewerState extends State<ImageViewer> {
+  final imagePicker = ImagePicker();
+
+  File? file;
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: () {},
+      onTap: () async {
+        final image = await imagePicker.pickImage(source: ImageSource.gallery);
+
+        setState(() {
+          if (image != null) {
+            file = File(image.path);
+          }
+        });
+      },
       child: Container(
         height: 100,
         width: double.infinity,
