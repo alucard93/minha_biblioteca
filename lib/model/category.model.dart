07d1bdd5dd5cb712e1f1418fd06fbd022e31c21e
Marcus Vinicius Nascimento Ferreira <dev.mvinicius@gmail.com>
2026-03-24 10:03:11 -0300

    feat: Implement Hive model for Category with TypeAdapter and update dependencies

--- lib/model/category.model.dart
+++ lib/model/category.model.dart
@@ -1,6 +1,16 @@
+import 'package:hive/hive.dart';
+
+part 'category.model.g.dart';
+
+@HiveType(typeId: 1)
 class Category {
+  @HiveField(0)
   final String id;
+
+  @HiveField(1)
   final String name;
+
+  @HiveField(2)
   final String? filePath;
 
   Category({required this.id, required this.name, this.filePath});
