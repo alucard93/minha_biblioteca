b4baf490a8ff2a5508dca527f439a03dc269de27
Marcus Vinicius Nascimento Ferreira <dev.mvinicius@gmail.com>
2026-04-11 19:12:52 -0300

    feat: Refactor CategoryRepository to include boxCategories initialization and fix addNewCategory method

--- lib/repositories/category.repository.dart
+++ lib/repositories/category.repository.dart
@@ -1,7 +1,15 @@
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:minha_biblioteca/model/category.model.dart';
 
 class CategoryRepository {
-  Future<void> addNewCategory(Category caategory) async {
-    
+  final Box<List> boxCategories;
+
+  CategoryRepository(this.boxCategories);
+
+  Future<void> addNewCategory(Category category) async {
+    final categories = boxCategories.get('categories', defaultValue: [])!;
+    categories.add(category);
+    await boxCategories.put('categories', categories);
+    await Future.delayed(const Duration(seconds: 2));
   }
 }
